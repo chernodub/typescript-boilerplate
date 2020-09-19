a24772e80678d949d54db08c2c5a018ecedb6ffe
Viktor Chernodub <chernodub98@gmail.com>
2020-09-19 09:16:42 +0700

    fix webpack config for ts

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
