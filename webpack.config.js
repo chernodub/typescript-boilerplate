6b79079c2ed525cac60fb738343b269f5df28d7a
Viktor Chernodub <chernodubv@gmail.com>
2020-12-24 09:49:04 +0700

    revise structure and update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
-  entry: './src/index.ts',
+  entry: {
+    css: './src/css/index.css',
+    main: './src/index.ts',
+  },
   resolve: {
     extensions: ['.ts', '.js'],
   },
@@ -12,10 +18,21 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      cache: false,
+    }),
+  ],
 };
